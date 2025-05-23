0c4c6412388927d4dd6941702abb6dd34c668c4b
lucile bessac <lucilebessac@free.fr>
2025-05-23 16:49:27 +0200

    modif EE

--- src/index.py
+++ src/index.py
@@ -16,28 +16,60 @@ def indexe_enonces_elem(corpus, req, param):
     grewpy.set_config("sud")
     request = grewpy.Request(req)
     liste_match=corpus.search(request)
+    print("liste_match", liste_match)
+    
     liste_des_enonces_elem=[]
     n = 0
+
+    # Un match est un X et un Y correspondant à la requête
+    # match = {'sent_id': '3', 'matching': {'nodes': {'Y': '2', 'X': '4'}, 'edges': {}}}
+
+    # for match in liste_match:
+    #     n+= 1
+    #     sent_id = match["sent_id"]
+    #     print(f"sent_id = {sent_id}")
+
+    #     # On récupère le numéro de node de Y et X
+    #     #  {'nodes': {'Y': '2', 'X': '4'}}
+    #     liste_node=[]
+    #     for node_number in match["matching"]["nodes"].values():
+    #         liste_node.append(int(node_number))
+    #     liste_node.sort()
+        
+    #     # On récupère le 'param' demandé (ici 'lemma') pour chaque node
+    #     liste_forms = []
+    #     for node_number in liste_node:
+    #         # print("corpus[sent_id].features", corpus[sent_id].features)
+    #         if param in corpus[sent_id].features[str(node_number)]:
+    #             # print(f"node_number = {node_number}")
+    #             # print(f"param = {param}")
+    #             # print(f"corpus[sent_id].features[str(node_number)] = {corpus[sent_id].features[str(node_number)]}")
+                
+    #             liste_forms.append(corpus[sent_id].features[str(node_number)][param])
+    #     dico_un_enonce_elem = {
+    #         "id_EE":n,
+    #         "id_sent": sent_id,
+    #         "formes": liste_forms,
+    #     }
+    #     liste_des_enonces_elem.append(dico_un_enonce_elem)
+    # return liste_des_enonces_elem
+
     for match in liste_match:
-        n+= 1
-        sent_id = match["sent_id"]
-        print(f"sent_id = {sent_id}")
-        ##on affiche le text de la phrase
-        liste_node=[]
-        for node_number in match["matching"]["nodes"].values():
-            liste_node.append(int(node_number))
-        liste_node.sort()
-        liste_forms = []
-        for node_number in liste_node:
-            if param in corpus[sent_id].features[str(node_number)]:
-                liste_forms.append(corpus[sent_id].features[str(node_number)][param])
+        n+=1
+        sent_id= match["sent_id"]
+        formes_EE = {
+            "N1": corpus[sent_id].features[str(match["matching"]["nodes"]["Y"])]["lemma"],
+            "V": corpus[sent_id].features[str(match["matching"]["nodes"]["X"])]["lemma"],
+        }
         dico_un_enonce_elem = {
-            "id":n,
-            "sent_id": sent_id,
-            "formes": liste_forms,
+            "id_EE":n,
+            "id_sent": sent_id,
+            "formes_EE": formes_EE,
         }
         liste_des_enonces_elem.append(dico_un_enonce_elem)
     return liste_des_enonces_elem
+
+    # X-->
 
 if __name__ == "__main__":
     # Exemple d'utilisation
@@ -46,7 +78,7 @@ if __name__ == "__main__":
     req = "pattern { X-[nsubj|obj|iobj|nsubj:pass|cop]->Y }"
     param = "lemma"
     liste_enonces_elem = indexe_enonces_elem(corpus, req, param)
-    print(liste_enonces_elem)
+    print("\n\nLISTE DES ÉNONCÉS ÉLÉMENTAIRES :\n\n",liste_enonces_elem, "\n\n")
     for element in liste_enonces_elem:
         print(element)
 
